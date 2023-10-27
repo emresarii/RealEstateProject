36fdf0be235d9370a00e99a3f356cd0661bc3166
emresarii <lme_sari@hotmail.com>
2023-10-27 23:01:07 +0300

    Homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,4 +3,19 @@
 </script>
 
 <template>
+  <div class="main-page-container">
+    <h1>Randevu düzenleme sistemine hoş geldiniz.</h1>
+    <h5>Randevular kısmından hali hazırda alınmış randevuları görebilir ve düzenleme yapabilirsiniz.</h5>
+    <h5>Randevu oluştur kısmından harita üzerinden posta kodu seçerek yeni randevu oluşturabilir, tahmini uzaklık verilerini görebilirsiniz.</h5>
+  </div>
 </template>
+
+<style scoped>
+.main-page-container{
+  display: flex;
+  flex-direction: column;
+  width: calc(100vw - 300px);
+  justify-content: center;
+  align-items: center;
+}
+</style>
